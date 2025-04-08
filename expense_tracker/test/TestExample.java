4c578ab2776943825dc14167da4afff11516e96c
Haley-Ogier <hogier@umass.edu>
2025-04-08 03:11:53 -0400

    Added Tests

--- expense_tracker/test/TestExample.java
+++ expense_tracker/test/TestExample.java
@@ -1,5 +1,6 @@
 // package test;
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
 import java.util.List;
@@ -8,6 +9,8 @@ import org.junit.Before;
 import org.junit.Test;
 
 import controller.ExpenseTrackerController;
+import model.AmountFilter;
+import model.CategoryFilter;
 import model.ExpenseTrackerModel;
 import model.Transaction;
 import view.ExpenseTrackerView;
@@ -77,4 +80,83 @@ public class TestExample {
         assertEquals(0.00, totalCost, 0.01);
     }
     
+
+    @Test
+    public void testAddTransaction_New() {
+        // Setup
+        assertEquals(0, model.getTransactions().size());
+
+        // Execution
+        boolean result = controller.addTransaction(50.0, "food");
+        assertTrue("Transaction should be added successfully", result);
+
+        // Validation
+        assertEquals("Transaction is added to the table", 1, model.getTransactions().size());
+        assertEquals("Total Cost is updated to 50.0", 50.0, getTotalCost(), 0.01);
+    }
+
+
+    @Test
+    public void testInvalidInputHandling() {
+        // Setup
+        assertEquals(0, model.getTransactions().size());
+        double startingTotal = getTotalCost();
+
+        // Execution
+        boolean negativeAmount = controller.addTransaction(-5.0, "food");
+        assertFalse("Should display error when adding a negative amount", negativeAmount);
+
+        boolean invalidCategoryResult = controller.addTransaction(20, "sports");
+        assertFalse("Should display error when adding an invalid category", invalidCategoryResult);
+
+        // Validation
+        assertEquals("No transactions added to table", 0, model.getTransactions().size());
+        assertEquals("Total cost remains unchanged", startingTotal, getTotalCost(), 0.01);
+    }
+
+
+    @Test
+    public void testFilterByAmount() {
+        // Setup
+        controller.addTransaction(15.00, "bills");     
+        controller.addTransaction(45.07, "travel");  
+        controller.addTransaction(15.00, "food"); 
+        controller.addTransaction(15.32, "travel"); 
+        controller.addTransaction(15, "other"); 
+
+        // Execution
+        AmountFilter amountFilter = new AmountFilter(15.00);
+        List<Transaction> filteredList = amountFilter.filter(model.getTransactions());
+
+        // Validation
+        assertEquals("Should find 3 transactions with amount=15.00", 3, filteredList.size());
+
+        for (Transaction t : filteredList) {
+            assertEquals("Amount should be 15.00", 15.00, t.getAmount(), 0.01);
+        }
+    }
+
+
+    @Test
+    public void testFilterByCategory() {
+        // Setup
+        controller.addTransaction(15.00, "bills");     
+        controller.addTransaction(45.07, "travel");  
+        controller.addTransaction(15.00, "food"); 
+        controller.addTransaction(15.32, "travel"); 
+        controller.addTransaction(15, "other"); 
+
+        // Execution
+        CategoryFilter categoryFilter = new CategoryFilter("travel");
+        List<Transaction> filteredList = categoryFilter.filter(model.getTransactions());
+
+        // Validation
+        assertEquals("Should find 2 transactions with category=travel", 2, filteredList.size());
+
+        for (Transaction t : filteredList) {
+            assertEquals("Category should be 'travel'", "travel", t.getCategory());
+        }
+    }
+
+
 }
